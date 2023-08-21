bdfddb1dc9e89f2cb52702cfbf75e0b5252640c6
Krishit <krishitdave2317@gmail.com>
2023-08-21 19:29:16 +0530

    Add project files.

---
+++ Infrastructure.Data/MachineDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class MachineDBContext : DbContext
+    {
+        public MachineDBContext(DbContextOptions<MachineDBContext> options) : base(options) { }
+
+        public DbSet<Car> cars{ get; set; }
+    }
+}
---
+++ Practice.Domain/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Domain
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string wheel { get; set; }
+        public string Body { get; set; }
+        public string Mirror { get; set; }
+        public string Lights { get; set; }
+    }
+}
